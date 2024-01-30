c8e7ae38658ca284cc654cffb674fa38e3cada49
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-30 23:29:56 +0200

    In progress.....

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ let button = document.querySelector('#button');
  
 
 function selectGrades() {
-    const popUpNumber = parseInt(prompt('Enter the number of squares on each side: '));
+    const popUpNumber = 5176//parseInt(prompt('Enter the number of squares on each side: '));
 
     let container = document.querySelector('.container');
     container.innerHTML ='';
 
-   if(popUpNumber <= 100) {
+   if(popUpNumber <= 6400) {
     for(let i= 0; i < popUpNumber; i++){
         let div = document.createElement('div');
         div.classList.add('grade');
         container.appendChild(div);
     
-
     div.addEventListener('mouseover', (e) => {
-       e.target.style.backgroundColor = 'white';
+        let divOuver = '0123456789ABCDEF';
+        let backgroundColor = '#';
+        for (let i = 0; i < 6; i++) {
+            backgroundColor += divOuver[Math.floor(Math.random() * 16)];
+        }
+       e.target.style.backgroundColor = backgroundColor;
 
        setTimeout(() => {
         e.target.style.backgroundColor = 'black';
-        }, 500)
+        }, 30000)
     }); 
     } 
    } else {
