4f162830a36b79f0a233afa7c9f15d73cb9d12c5
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-31 16:29:28 +0200

    Project finished

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
+let body = document.querySelector('body');
 let button = document.querySelector('#button');
- 
+let container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
 
-function selectGrades() {
-    const popUpNumber = 5176//parseInt(prompt('Enter the number of squares on each side: '));
-
-    let container = document.querySelector('.container');
-    container.innerHTML ='';
-
-   if(popUpNumber <= 6400) {
-    for(let i= 0; i < popUpNumber; i++){
-        let div = document.createElement('div');
-        div.classList.add('grade');
-        container.appendChild(div);
-    
-    div.addEventListener('mouseover', (e) => {
-        let divOuver = '0123456789ABCDEF';
-        let backgroundColor = '#';
-        for (let i = 0; i < 6; i++) {
-            backgroundColor += divOuver[Math.floor(Math.random() * 16)];
-        }
-       e.target.style.backgroundColor = backgroundColor;
-
-       setTimeout(() => {
-        e.target.style.backgroundColor = 'black';
-        }, 30000)
-    }); 
-    } 
-   } else {
-    alert('The number should not be greater than 100!');
+function hiddenClasslist() {
+    let hiddenDiv = document.querySelector('#content-container');
+    hiddenDiv.classList.add('hidden');
+    button.style.cssText = ` 
+    position: fixed; 
+    padding: 10px 15px; 
+    color: gray; 
+    left: 20px;
+    top: 20px
+    background-color: #800020; 
+    font-size: 17px; 
+    border-radius: 16px;`;
+    button.textContent ='Create new grid';
 }
-}   
 
-button.addEventListener('click', selectGrades);
+function createGrid(numberOfSquares) {
+    container.innerHTML = ''; 
+
+    for (let i = 0; i < numberOfSquares; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        row.style.flex = `1 1 calc(100% / ${numberOfSquares} - 1px;)`;
+        container.appendChild(row);
+
+        for (let j = 0; j < numberOfSquares; j++) {
+            let div = document.createElement('div');
+            div.classList.add('grade');
+            div.style.flex = `1 1 calc(100% / ${numberOfSquares} - 1px;)`;
+            row.appendChild(div);
+
+
+        }
+    }
+
+    container.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('grade')) {
+            let divOuver = '0123456789ABCDEF';
+            let backgroundColor = '#';
+            for (let i = 0; i < 6; i++) {
+                backgroundColor += divOuver[Math.floor(Math.random() * 16)];
+                }
+                e.target.style.backgroundColor = backgroundColor;
+            }
+
+            setTimeout( () => {
+                e.target.style.backgroundColor = ''
+            }, 3000)
+    });
+
+}
+
+button.addEventListener('click', () => {
+    let newNumber = parseInt(prompt('Enter the number of squares on each side: '));
+    
+    if (!isNaN(newNumber) && newNumber <= 45 && newNumber > 0) {
+        createGrid(newNumber);
+        hiddenClasslist();
+    } else {
+        alert('You can only enter a valid number between 1 and 45.');
+    }
+});
+
